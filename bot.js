4c3c52ed12d7461cc42fc125ee66a554f63e4388
Discord.js Arabian Developer <32039975+Bowlingtoolkit@users.noreply.github.com>
2019-01-03 01:19:47 -0800

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+
+client.on("ready", async  => {
+setInterval(function(){
+client.channels.find('id', '520856638127603719').setName("W");
+client.channels.find('id', '520856638127603719').setName("We");
+client.channels.find('id', '520856638127603719').setName("Wel");
+client.channels.find('id', '520856638127603719').setName("Welc");
+client.channels.find('id', '520856638127603719').setName("Welco");
+client.channels.find('id', '520856638127603719').setName("Welcom");
+client.channels.find('id', '520856638127603719').setName("Welcome");
+client.channels.find('id', '520856638127603719').setName("Welcome T");
+client.channels.find('id', '520856638127603719').setName("Welcome To");
+client.channels.find('id', '520856638127603719').setName("Welcome To S");
+client.channels.find('id', '520856638127603719').setName("Welcome To Su");
+client.channels.find('id', '520856638127603719').setName("Welcome To Sub");
+client.channels.find('id', '520856638127603719').setName("Welcome To Suba");
+client.channels.find('id', '520856638127603719').setName("Welcome To Suba");
+client.channels.find('id', '520856638127603719').setName("Welcome To Subar");
+client.channels.find('id', '520856638127603719').setName("Welcome To Subaru");
+  }, 3000);
+});
+
+
+client.login(process.env.BOT_TOKEN)
