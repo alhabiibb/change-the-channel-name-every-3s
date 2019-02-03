4a979245be19804be72999e5d098689de8b0e723
alhabiibb <46806624+alhabiibb@users.noreply.github.com>
2019-02-04 01:43:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,22 +4,22 @@ const client = new Discord.Client()
 
 client.on("ready", async  => {
 setInterval(function(){
-client.channels.find('id', '520856638127603719').setName("W");
-client.channels.find('id', '520856638127603719').setName("We");
-client.channels.find('id', '520856638127603719').setName("Wel");
-client.channels.find('id', '520856638127603719').setName("Welc");
-client.channels.find('id', '520856638127603719').setName("Welco");
-client.channels.find('id', '520856638127603719').setName("Welcom");
-client.channels.find('id', '520856638127603719').setName("Welcome");
-client.channels.find('id', '520856638127603719').setName("Welcome T");
-client.channels.find('id', '520856638127603719').setName("Welcome To");
-client.channels.find('id', '520856638127603719').setName("Welcome To S");
-client.channels.find('id', '520856638127603719').setName("Welcome To Su");
-client.channels.find('id', '520856638127603719').setName("Welcome To Sub");
-client.channels.find('id', '520856638127603719').setName("Welcome To Suba");
-client.channels.find('id', '520856638127603719').setName("Welcome To Suba");
-client.channels.find('id', '520856638127603719').setName("Welcome To Subar");
-client.channels.find('id', '520856638127603719').setName("Welcome To Subaru");
+client.channels.find('id', '541419701775892513').setName("W");
+client.channels.find('id', '541419701775892513').setName("We");
+client.channels.find('id', '541419701775892513').setName("Wel");
+client.channels.find('id', '541419701775892513').setName("Welc");
+client.channels.find('id', '541419701775892513').setName("Welco");
+client.channels.find('id', '541419701775892513').setName("Welcom");
+client.channels.find('id', '541419701775892513').setName("Welcome");
+client.channels.find('id', '541419701775892513').setName("Welcome T");
+client.channels.find('id', '541419701775892513').setName("Welcome To");
+client.channels.find('id', '541419701775892513').setName("Welcome To T");
+client.channels.find('id', '541419701775892513').setName("Welcome To r");
+client.channels.find('id', '541419701775892513').setName("Welcome To i");
+client.channels.find('id', '541419701775892513').setName("Welcome To l");
+client.channels.find('id', '541419701775892513').setName("Welcome To Trick");
+client.channels.find('id', '541419701775892513').setName("Welcome To Trick");
+client.channels.find('id', '541419701775892513').setName("Welcome To Trick");
   }, 3000);
 });
 
